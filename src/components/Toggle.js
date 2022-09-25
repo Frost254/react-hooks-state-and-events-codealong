f316c37446960e7e6d7ecac4e60b1960e867c467
Derrick Nyasio <dnyasio2014@gmail.com>
2022-09-26 00:25:58 +0300

    Assignment Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+    const [toggle, setToggle] = useState(false);
+
+    function handleClick() {
+        setToggle((toggle) => !toggle)
+    }
+
+    const color = toggle ? "green" : "red";
+    return <button style = {
+        { background: color }
+    }
+    onClick = { handleClick } > { toggle ? "ON" : "OFF" } < /button>;
 }
 
 export default Toggle;
